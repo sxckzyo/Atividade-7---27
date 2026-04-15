a2b78942faa69f2eef7a7fcb004f8e174881fb09
sxckzyo <rafael.kiss.fortunato@escola.pr.gov.br>
2026-04-15 11:56:32 -0300

    Add files via upload

---
+++ ex09.py
@@ -0,0 +1,10 @@
+# Verificando se um número é positivo ou negativo
+numero = int(input("Digite um número: "))
+
+# ciando uma condição
+if numero > 0:
+    print("Número positivo")
+elif numero == 0:
+    print("Número é zero")
+else:
+    print("Número negativo")
---
+++ ex11.py
@@ -0,0 +1,7 @@
+# Número par ou ímpar
+numero = int(input("Digite um número: "))
+
+if numero % 2 == 0:
+    print("Número par")
+else:
+    print("Número ímpar")
