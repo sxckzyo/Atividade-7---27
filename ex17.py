82a65537cdf62a0aea32f620b5f21c51b3e50e62
srnslk <rodrigo.cavalcanti.filho@escola.pr.gov.br>
2026-04-16 12:42:02 -0300

    Add files via upload

---
+++ ex17.py
@@ -0,0 +1,9 @@
+#Login simples 
+usuario = input("Usuário: ")
+senha = input("Sistema: ")
+
+if usuario == "admin" and senha == "123":
+    print("Login realizado")
+else:
+    print("Usuário ou senha incorretos")
+    
---
+++ ex19.py
@@ -0,0 +1,11 @@
+# Prioridade
+idade = int(input("Idade: "))
+gestante = input("é gestante (sim ou não): ")
+
+if idade >= 60 or gestante == "sim":
+    print("Senha preferencial")
+
+else: 
+    print("Atendimento normal")
+
+    
